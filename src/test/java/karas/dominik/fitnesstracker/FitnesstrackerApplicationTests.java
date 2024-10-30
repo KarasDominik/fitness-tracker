09cd7f9fdf6e7cdc7e247b9955120f05a837c19c
Dominik <dominik.karas@clurgo.com>
2024-10-30 23:24:00 +0100

    Initial commit

---
+++ src/test/java/karas/dominik/fitnesstracker/FitnesstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package karas.dominik.fitnesstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesstrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
